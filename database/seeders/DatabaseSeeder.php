0763eebbc607fc3dad27c273766c49fa843e521c
Nanda Safiq Alfiansyah <nandasafiqalfiansyah@gmail.com>
2025-12-27 11:43:57 +0700

    Update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminUserSeeder::class,
+            AutoReplyConfigSeeder::class,
+            CsTrainingDataSeeder::class,
         ]);
     }
 }
